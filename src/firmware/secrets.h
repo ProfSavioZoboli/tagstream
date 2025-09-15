185f13743cb5e47d93c3aad4d5d4ae9ff2f11547
SAVIO EDUARDO ZOBOLI <savio_zoboli@estudante.sc.senai.br>
2025-09-15 16:05:54 -0300

    feat: adicionado gitignore para arquivo de segredos

--- src/firmware/secrets.h
+++ src/firmware/secrets.h
@@ -9,7 +9,9 @@ static const char* password = "Smart4.0";
 static const char* mqtt_server = "189.8.205.50";
 static const char* mqtt_user = "profblu";
 static const char* mqtt_pass = "ProFBlu@hotdog";
+
 static const char* mqtt_topic_req_usrs = "/indaial/req/usuarios";
 static const char* mqtt_topic_res_usrs = "/indaial/res/usuarios";
+static const char* mqtt_topic_log_usuarioLogado = "/indaial/log/usuarioLogado";
 
 #endif

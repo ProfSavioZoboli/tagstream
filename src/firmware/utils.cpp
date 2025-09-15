412c27d507f43e1181fd7589857ae6f12e3c2226
SAVIO EDUARDO ZOBOLI <savio_zoboli@estudante.sc.senai.br>
2025-09-15 16:02:20 -0300

    feat: adicionado arquivo de conversores e cálculos genéricos

--- src/firmware/utils.cpp
+++ src/firmware/utils.cpp
@@ -8,3 +8,27 @@ long long getTimestampAtual(){
     auto milis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
     return milis;
 }
+
+bool hexStringToByteArray(const char* hexStr, byte* byteArray, int arraySize) {
+  int len = strlen(hexStr);
+  if (len != arraySize * 2) return false;
+
+  for (int i = 0; i < arraySize; i++) {
+    char hexPair[3] = { hexStr[i * 2], hexStr[i * 2 + 1], '\0' };
+    byteArray[i] = (byte)strtol(hexPair, NULL, 16);
+  }
+  return true;
+}
+
+// Converte um array de bytes para uma string hexadecimal
+void byteArrayToHexString(byte* byteArray, int arraySize, char* outputBuffer, int bufferSize) {
+    if (bufferSize < (arraySize * 2 + 1)) {
+        // Buffer de saída é muito pequeno
+        return;
+    }
+    
+    for (int i = 0; i < arraySize; i++) {
+        sprintf(outputBuffer + (i * 2), "%02X", byteArray[i]);
+    }
+    outputBuffer[arraySize * 2] = '\0'; // Adiciona o terminador nulo
+}
--- src/firmware/utils.h
+++ src/firmware/utils.h
@@ -2,6 +2,8 @@
 #define UTILS_H
 
 long long getTimestampAtual();
+bool hexStringToByteArray(const char* hexStr, byte* byteArray, int arraySize);
+void byteArrayToHexString(byte* byteArray, int arraySize, char* outputBuffer, int bufferSize);
 
 
 #endif
